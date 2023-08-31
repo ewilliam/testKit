6cfa4c337c72dfe7ae2d9ca4c8ed2ef1fe174f6d
William Albright <ping@ewilli.am>
2023-08-30 18:33:06 -0700

    add fetch in hooks.server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,11 @@
+export async function handle({ event, resolve }) {
+    // only this will trigger handleFetch
+    // but fetch here in handle will also crash the app
+    // const fetchReq = await event.fetch('/api');
+    const response = await resolve(event);
+    return response;
+}
+
 export async function handleFetch({ request, fetch }) {
     // nothing happens
     console.log('handleFetch', request.url);
